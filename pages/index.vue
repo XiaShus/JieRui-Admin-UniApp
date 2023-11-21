b5f0c0e1ac515074a276dc0d58cca545b487d72d
LiZhiQ <18196671484@163.com>
2023-11-21 17:29:48 +0800

    feat: 1.首页临时修改

--- pages/index.vue
+++ pages/index.vue
@@ -3,33 +3,45 @@
 
     <!-- 顶部搜索 -->
     <view class="">
-      <u-search :showAction="true" actionText="搜索" :animation="false"></u-search>
+      <u-row>
+        <u-col :span="11">
+          <u-search
+              v-model="keyword"
+              :show-action="false"
+              @change="change"
+          ></u-search>
+        </u-col>
+        <u-col span="1" >
+          <u-icon name="bell" color="#FFFFFF" size="30"></u-icon>
+        </u-col>
+      </u-row>
+
     </view>
     <!-- 快捷功能区 -->
-    <view class="">
-      <view>
-        <u-icon name="photo" color="#2979ff" size="100"></u-icon>
-        <text></text>
-      </view>
-      <view>
-        <u-icon name="photo"></u-icon>
-      </view>
-      <view>
-        <u-icon name="photo"></u-icon>
-      </view>
-      <view>
-        <u-icon name="photo"></u-icon>
-      </view>
+    <view class="core-func-box">
+      <u-row justify="center" customStyle="margin-bottom: 10px">
+        <u-col align="center" span="3" v-for="v in coreFunction">
+          <view style="text-align: center">
+            <u-icon :name="v.icon" color="#FFFFFF" size="100"></u-icon>
+            <text>{{ v.title }}</text>
+          </view>
+        </u-col>
+      </u-row>
     </view>
     <!-- 常用功能区 -->
-    <view class="">
-
+    <view class="common-func-box">
+      <u-row style="flex-wrap: wrap" customStyle="margin-bottom: 10px">
+        <u-col span="2.4" v-for="v in commonFunction">
+          <view style="text-align: center">
+            <u-icon :name="v.icon" color="#00000" size="50"></u-icon>
+            <text>{{ v.title }}</text>
+          </view>
+        </u-col>
+      </u-row>
     </view>
     <!-- 滚动公告 -->
-    <view class="">
-      <view class="notice-box">
+    <view class="notice-box">
 
-      </view>
     </view>
     <!-- 混合功能区 -->
     <view class="">
@@ -39,10 +51,64 @@
 </template>
 
 <script>
+import UniCard from "../uni_modules/uni-card/components/uni-card/uni-card.vue";
+
 export default {
+  components: {UniCard},
   data() {
     return {
-      keyword: '遥看瀑布挂前川'
+      keyword: '',
+      coreFunction: [
+        {
+          icon: 'photo',
+          title: '123',
+          path: ''
+        },
+        {
+          icon: 'scan',
+          title: '扫一扫',
+          path: ''
+        },
+        {
+          icon: 'photo',
+          title: '123',
+          path: ''
+        },
+        {
+          icon: 'photo',
+          title: '123',
+          path: ''
+        },
+      ],
+      commonFunction: [
+        {
+          icon: 'photo',
+          title: '质量管理',
+          path: ''
+        },
+        {
+          icon: 'photo',
+          title: '安全管理',
+          path: ''
+        },
+        {
+          icon: 'photo',
+          title: '网盘管理',
+          path: ''
+        },
+        {
+          icon: 'photo',
+          title: '任务中心',
+          path: ''
+        },
+        {
+          icon: 'photo',
+          title: '数据中心',
+          path: ''
+        },
+      ],
+      noticeData: [],
+
     }
   },
   onLoad: function () {
@@ -57,5 +123,22 @@ export default {
 
 .notice-box {
   background-color: #1cbbb4;
+  border-radius: 20px;
+  width: 100%;
+  height: 20px;
+}
+
+.core-func-box {
+  color: #FFFFFF;
+}
+
+.common-func-box {
+  padding: 20px;
+  margin-bottom: 20px;
+  border-radius: 20px;
+  width: 100%;
+  background-color: #FFFFFF;
+  color: #000000;
+  flex-warp: warp;
 }
 </style>
